3c69bb918437612b682daf0716a62cc7db299074
Viper373 <2020311228@bipt.edu.cn>
2025-03-05 05:15:29 +0800

    🍟重构数据采集代码

--- crawling_data/concat_json.py
+++ crawling_data/concat_json.py
@@ -1,4 +1,14 @@
+# -*- coding:utf-8 -*-
+# @Project        :LPL
+# @FileName       :concat_json.py
+# @Time           :2025/3/5 00:14
+# @Software       :PyCharm
+# @Author         :Viper373
+# @Index          :https://viper3.top
+# @Blog           :https://blog.viper3.top
+
 import json
+import logging
 
 # 读取hero_list.json文件
 with open('../data/json/hero_list.json', 'r', encoding='utf-8') as f:
@@ -9,10 +19,10 @@ with open('../data/json/hero_logo.json', 'r', encoding='utf-8') as f:
     hero_logo = json.load(f)
 
 # 创建一个heroId到heroLogo的映射
-hero_logo_dict = {str(hero['heroId']): hero['heroLogo'] for hero in hero_logo['data']}
+hero_logo_dict = {str(hero['heroId']): hero['heroLogo'] for hero in hero_logo}
 
 # 更新hero_list.json中的数据，添加heroLogo字段
-for hero in hero_list:
+for hero in hero_list.get('hero'):
     hero_id = hero['heroId']
     if hero_id in hero_logo_dict:
         hero['heroLogo'] = hero_logo_dict[hero_id]
@@ -21,4 +31,4 @@ for hero in hero_list:
 with open('../data/json/hero_info.json', 'w', encoding='utf-8') as f:
     json.dump(hero_list, f, ensure_ascii=False, indent=4)
 
-print("合并完成，结果已保存到hero.json文件中。")
+logging.info(f"hero_info.json 合并完成 丨 {hero_list.get('version')} 丨 {hero_list.get('fileTime')}")
